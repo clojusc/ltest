56e78df75fc25b66a84f8924d7eb846f7f9d6c0d
Duncan McGreggor <oubiwann@gmail.com>
2017-06-10 10:00:08 -0500

    Initial files.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject ltest "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/clojusc/ltest"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ test/ltest/core_test.clj
@@ -0,0 +1,7 @@
+(ns ltest.core-test
+  (:require [clojure.test :refer :all]
+            [ltest.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
