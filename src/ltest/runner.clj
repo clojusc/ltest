cf3c495db4a0b932a0cb135980474d0ec7ab4ddd
Duncan McGreggor <oubiwann@gmail.com>
2017-06-13 18:36:34 -0500

    The defmulti needs to be in ltest too, in order to not override.

--- src/ltest/runner.clj
+++ src/ltest/runner.clj
@@ -41,7 +41,6 @@
    testing-vars-str
    ;; multimethods
    assert-expr
-   report
    use-fixtures
    ;; macros
    are
@@ -137,6 +136,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;   Reporter implementation/overrides   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmulti
+  ^{:doc "Generic reporting function, may be overridden to plug in
+   different report formats (e.g., TAP, JUnit).  Assertions such as
+   'is' call 'report' to indicate results.  The argument given to
+   'report' will be a map with a :type key.  See the documentation at
+   the top of test_is.clj for more information on the types of
+   arguments for 'report'."
+     :dynamic true}
+  report :type)
 
 (defmethod report :default
   [m]
