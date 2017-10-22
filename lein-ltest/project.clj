18bfd27fbea8cbe525c7562acb9b10a455900f89
Duncan McGreggor <oubiwann@gmail.com>
2017-10-21 20:56:56 -0500

    Updated travis config and lein plugin to build.

--- lein-ltest/project.clj
+++ lein-ltest/project.clj
@@ -4,4 +4,10 @@
   :license {
     :name "Eclipse Public License"
     :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :aliases {
+    "build" ["do"
+      ["check"]
+      ["compile"]
+      ["clean"]
+      ["jar"]]})
