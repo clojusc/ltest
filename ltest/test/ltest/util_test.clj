e30f72b71c4db7413112d4c332f178704c3416f4
Duncan McGreggor <oubiwann@gmail.com>
2019-10-10 21:56:04 -0500

    Added tests.

---
+++ ltest/test/ltest/util_test.clj
@@ -0,0 +1,17 @@
+(ns ltest.util-test
+  (:require
+    [clojure.test :refer [are deftest is testing]]
+    [ltest.core :refer [run-test run-tests run-suite run-suites]]
+    [ltest.util :as util]))
+
+(deftest nano->human-readable
+  (is (= "1ns" (util/nano->human-readable 1)))
+  (is (= "10ns" (util/nano->human-readable 10)))
+  (is (= "100ns" (util/nano->human-readable 100)))
+  (is (= "1μs" (util/nano->human-readable 1000)))
+  (is (= "10μs" (util/nano->human-readable 10000)))
+  (is (= "100μs" (util/nano->human-readable 100000)))
+  (is (= "1ms" (util/nano->human-readable 1000000)))
+  (is (= "10ms" (util/nano->human-readable 10000000)))
+  (is (= "100ms" (util/nano->human-readable 100000000)))
+  (is (= "1s" (util/nano->human-readable 1000000000))))
