6655ad2af3517d35a1ec3066c6b12f6af59d2436
Duncan McGreggor <oubiwann@gmail.com>
2019-10-10 22:53:09 -0500

    Display test context with assertion, if defined.

--- ltest/test/ltest/util_test.clj
+++ ltest/test/ltest/util_test.clj
@@ -5,13 +5,16 @@
     [ltest.util :as util]))
 
 (deftest nano->human-readable
-  (is (= "1ns" (util/nano->human-readable 1)))
-  (is (= "10ns" (util/nano->human-readable 10)))
-  (is (= "100ns" (util/nano->human-readable 100)))
-  (is (= "1μs" (util/nano->human-readable 1000)))
-  (is (= "10μs" (util/nano->human-readable 10000)))
-  (is (= "100μs" (util/nano->human-readable 100000)))
-  (is (= "1ms" (util/nano->human-readable 1000000)))
-  (is (= "10ms" (util/nano->human-readable 10000000)))
-  (is (= "100ms" (util/nano->human-readable 100000000)))
+  (testing "Nanosecond checks"
+    (is (= "1ns" (util/nano->human-readable 1)))
+    (is (= "10ns" (util/nano->human-readable 10)))
+    (is (= "100ns" (util/nano->human-readable 100))))
+  (testing "Microsecond checks"
+    (is (= "1μs" (util/nano->human-readable 1000)))
+    (is (= "10μs" (util/nano->human-readable 10000)))
+    (is (= "100μs" (util/nano->human-readable 100000))))
+  (testing "Millisecond checks"
+    (is (= "1ms" (util/nano->human-readable 1000000)))
+    (is (= "10ms" (util/nano->human-readable 10000000)))
+    (is (= "100ms" (util/nano->human-readable 100000000))))
   (is (= "1s" (util/nano->human-readable 1000000000))))
